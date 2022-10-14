178fc93924bc8f0c8cd800d55077881311a942bb
Gustavo Godoy <gustavocoliver@gmail.com>
2022-10-14 02:46:48 -0300

    Adicionando filtro de pesquisa por data

--- email/src/main/java/br/com/fiap/email/repository/EmailEnviadoRepository.java
+++ email/src/main/java/br/com/fiap/email/repository/EmailEnviadoRepository.java
@@ -4,4 +4,7 @@ import br.com.fiap.email.model.EmailEnviadoModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EmailEnviadoRepository extends MongoRepository<EmailEnviadoModel, String> {
+  
+   @Query("{dataEnvio: /^?0/}")
+    List<EmailEnviadoModel> obterPorData(String dataEnvio);
 }
